e844dd69b25ea59f0399f47514de36ff355b20c2
abhutan <abhutan@ncsu.edu>
2014-02-28 03:41:00 -0500

    Update fizzbuzz.c
    Added comments for the program, removed unwanted header file

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 int main()
 {
 	int ctr;
 	for(ctr=1;ctr<=100;ctr++){
 		if(ctr%15==0)
-			printf("FizzBuzz\n");
+			printf("FizzBuzz\n"); //Number is divisible by both 3 and 5
 		else if(ctr%3==0)
-			printf("Fizz\n");
+			printf("Fizz\n"); // Number is divisible by only 3
 		else if(ctr%5==0)
-			printf("Buzz\n");
+			printf("Buzz\n"); // number is divisible by only 5
 		else
 			printf("%d\n",ctr);
 	}
